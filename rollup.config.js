5b6e269a45b20f740e17c92a9b7234958a6dbe05
GYUJEONG HWANG <octave08b@gmail.com>
2021-11-09 19:12:32 +0900

    feat: import json file

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
+import json from '@rollup/plugin-json'
+
 export default {
   input: 'src/main.js',
   output: {
     file: './build/bundle.js',
     format: 'cjs'
-  }
+  },
+  plugins: [json()]
 };
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
+import { version } from '../package.json';
 import foo from './foo.js';
+
 export default function () {
   console.log(foo);
+  console.log('version ' + version);
 }

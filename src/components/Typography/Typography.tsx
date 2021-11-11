01ffa892827396a212728348d9073cbbc2123176
Gyujeong Hwang <octave08b@gmail.com>
2021-11-11 05:25:08 +0000

    feat: integrate emotion

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -1,3 +1,9 @@
-export const Typography = () => {
-  <div>hello</div>
-}
+import styled from "@emotion/styled";
+import { typography, TypographyProps } from "styled-system";
+
+export const Typography = styled.span<TypographyProps>`
+  color: ${(props) => props.theme.colors.primary};
+  font-weight: 400;
+  line-height: 146%;
+  ${typography};
+`;

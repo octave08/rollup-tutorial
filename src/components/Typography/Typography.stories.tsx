cefe3231582d510abb574ef5c5076fb2305825d9
GYUJEONG HWANG <octave08b@gmail.com>
2021-11-11 19:39:32 +0900

    chore: remove unused code

--- src/components/Typography/Typography.stories.tsx
+++ src/components/Typography/Typography.stories.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Meta } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
 import styled from "@emotion/styled";
 
 import { Typography } from "./Typography";
